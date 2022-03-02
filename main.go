38bbfa7d48cfefa88d4e7bacc034073d589d8d67
liubo-work <liubo@work.com>
2022-03-02 11:04:45 +0800

    测试通过

---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+var f = flag.String("f", "", "-f=111.log")
+var l = flag.Int("l", 0, "-l=160000")
+var s = flag.Int("b", 0, "-b=16m")
+
+func main() {
+	flag.Parse()
+
+	if len(*f) == 0 {
+		flag.PrintDefaults()
+		panic("invalid filename")
+		return
+	}
+
+	if *l == 0 && *s == 0 {
+		flag.PrintDefaults()
+		panic("invalid args")
+		return
+	}
+
+	if *l > 0 {
+		SplitFileByLine(*f, *l)
+	} else if *s > 0 {
+		SplitFileByByteCount(*f, *s)
+	}
+}
+
+func SplitFileByLine(file string, lines int) {
+	var fileName = file
+	var ext = filepath.Ext(file)
+	if len(ext) == 0 {
+		ext = ".log"
+	}
+
+	var splitName = func(num int) string {
+		return file + ".part." + strconv.Itoa(num) + ext
+	}
+
+	// 删掉旧文件
+
+
+	fi, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	defer fi.Close()
+	var count = 0
+	br := bufio.NewReader(fi)
+	var bw = bytes.NewBuffer([]byte{})
+	var num = 0
+	for {
+		a, _, c := br.ReadLine()
+		if c == io.EOF {
+			if len(a) > 0 {
+				bw.Write(a)
+				bw.WriteString("\r\n")
+			}
+
+			ioutil.WriteFile(splitName(num), bw.Bytes(), 666)
+
+			break
+		}
+		bw.Write(a)
+		bw.WriteString("\r\n")
+		count++
+
+		if count >= lines {
+
+			ioutil.WriteFile(splitName(num), bw.Bytes(), 666)
+			bw.Reset()
+			num++
+			count = 0
+		}
+	}
+}
+
+func SplitFileByByteCount(file string, rawMaxSize int) {
+	var fileName = file
+	var ext = filepath.Ext(file)
+	if len(ext) == 0 {
+		ext = ".bin"
+	}
+
+	var splitName = func(num int) string {
+		return file + ".part." + strconv.Itoa(num) + ext
+	}
+
+	// 删掉旧文件
+
+
+	fi, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	defer fi.Close()
+	var count = 0
+	br := bufio.NewReader(fi)
+	var bw = bytes.NewBuffer([]byte{})
+	var num = 0
+	var buf = make([]byte, 4096)
+	for {
+		size, c := br.Read(buf)
+		if c == io.EOF {
+			if size > 0 {
+				bw.Write(buf[0:size])
+			}
+
+			ioutil.WriteFile(splitName(num), bw.Bytes(), 666)
+
+			break
+		}
+		bw.Write(buf[0:size])
+		count += size
+
+		if count >= rawMaxSize {
+
+			ioutil.WriteFile(splitName(num), bw.Bytes(), 666)
+			bw.Reset()
+			num++
+			count = 0
+		}
+	}
+}
